8c2f507dc0af689cc102683403eaf356473c7a7a
Matteotr9 <89576532+Matteotr9@users.noreply.github.com>
2022-08-31 16:54:13 +0200

    ajout de la form

--- items/src/main/java/fr/sio/items/controllers/MainController.java
+++ items/src/main/java/fr/sio/items/controllers/MainController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -34,6 +36,22 @@ public class MainController {
 		attrs.addFlashAttribute("msg", "Elément" + name + "ajouté");
 		return new RedirectView("/");
 
+	}
+
+//affichage formulaire
+	@GetMapping("add")
+	public RedirectView addForm(RedirectAttributes attrs) {
+		attrs.addFlashAttribute("form", true);
+		return new RedirectView("/");
+
+	}
+
+	@PostMapping("add")
+	public RedirectView postForm(@RequestParam String name, @SessionAttribute List<Element> items,
+			RedirectAttributes attrs) {
+		items.add(new Element(name));
+		attrs.addFlashAttribute("msg", "Element" + name + "ajouté");
+		return new RedirectView("/");
 	}
 
 }
